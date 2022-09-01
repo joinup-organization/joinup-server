8f08082f2ac561d5406c3f551df6110cfa47768c
Caio Rodrigues <c.alvesrodrig@gmail.com>
2022-09-01 20:26:37 -0300

    feat(#2): change code to the correct module

--- functions/src/Usecases/Projects/projects.service.ts
+++
@@ -1,7 +0,0 @@
-export interface IProjectController {
-    readonly createProject: () => any
-    readonly getProject: () => any
-    readonly listProjects: () => any
-    readonly listProjectsByPO: () => any
-    readonly listProjectsByUser: () => any
-}
